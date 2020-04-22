d9e7784ef838d199d2ff699eb2ebaa3c0ef8536a
SergeySolod <solodsergey287@gmail.com>
2020-04-22 23:15:11 +0300

    Add redux thunk

---
+++ client/src/api/CustomerApi.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const CustomersApi = {
+  getCustomers() {
+    return axios({
+
+    })
+  }
+}
--- client/src/redux/reducers/Customer-reducer.js
+++ client/src/redux/reducers/Customer-reducer.js
@@ -7,8 +7,23 @@ let initialState = {
 const CustomerReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_CUSTOMERS:
-      return state
+     return {
+       ...state,
+       customers: action.customers
+     }
     default:
       return state
   }
 }
+
+const setCustomers = customers => ({
+  type: SET_CUSTOMERS,
+  customers
+});
+
+export const setCustomersThunk = () => async (dispatch, getState) => {
+  let data = await CustomersApi.getCustomers();
+  console.log(data)
+};
+
+export default CustomerReducer
