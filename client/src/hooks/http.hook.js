45f738b6d1dd03ba9d0f9362dc28fadfa8bb0e1a
SergeySolod <solodsergey287@gmail.com>
2020-04-25 02:24:22 +0300

    Completed useHttp

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -5,17 +5,26 @@ export const useHttp = () => {
     const [error, setError] = useState(null)
 
     const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+            setLoading(true)
         try {
             const response = await fetch(url, {method, body, headers})
             //приводим после дожидания к форму json
             const data = await response.json()
             if (!response.ok) {
-
+                //выбрасываем ошибку если она есть
+                throw new Error(data.message || 'Что-то пошло не так')
             }
-        } catch (e) {
+            setLoading(false)
 
+            return data
+
+        } catch (e) {
+            setLoading(false)
+            //ошибка здесь появляется из инстанса throw
+            setError(e.message)
+            throw e
         }
-    })
+    }, [])
 
     return {loading, request, error}
 }
