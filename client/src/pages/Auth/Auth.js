48072c71fd907fcba657e9ea6b7411f36dff6a47
SergeySolod <solodsergey287@gmail.com>
2020-04-25 00:18:07 +0300

    Add input`s

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -2,12 +2,31 @@ import React from 'react'
 
 const Auth = props => {
     return (
-            <div className="row">
-                <div className="col s12">
-                    <p className="flow-text">Авторизация</p>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <div className="card blue darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title">Авторизация</span>
+                        <div>
+                            <div className="input-field">
+                                <input placeholder="Введите email" id="email" type="text"/>
+                                <label htmlFor="email">Email</label>
+                            </div>
+
+                            <div className="input-field">
+                                <input placeholder="Введите пароль" id="password" type="password"/>
+                                <label htmlFor="password">Пароль</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className='btn yellow darken-4 button-auth-log'>Войти</button>
+                        <button className='btn grey lighten-1 black-text'>Регистрация</button>
+                    </div>
                 </div>
             </div>
-)
+        </div>
+    )
 }
 
 export default Auth
