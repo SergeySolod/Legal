a3009529ea45bb3c2c798b7e5c23451c14186b6b
SergeySolod <solodsergey287@gmail.com>
2020-04-25 00:48:31 +0300

    Add mobile

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav>
       <div className="nav-wrapper #43a047 green darken-1">
-        <a href="javascript:void(0);" className="brand-logo right">LegalQuestions</a>
+        <a href="javascript:void(0)" className="brand-logo right">LegalQuestions</a>
         <ul id="nav-mobile" className="left hide-on-med-and-down">
           <li><NavLink to="/" exact>Главная</NavLink></li>
           <li><NavLink to="/questions" exact>Вопросы</NavLink></li>
