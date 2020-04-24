608ccc3b4cfecfb511478cd6f6146c62e888b550
SergeySolod <solodsergey287@gmail.com>
2020-04-25 00:35:52 +0300

    Add redux-form

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -1,29 +1,42 @@
 import React from 'react'
+import {Field, reduxForm} from "redux-form";
+import {Input} from "../../components/FormsControl";
+import {required} from "../../components/Validators";
+
+const AuthForm = props => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div className="card #43a047 green darken-1 card-auth">
+                <div className="card-content white-text">
+                    <span className="card-title">Авторизация</span>
+                    <div>
+                        <div className="input-field">
+                            <Field placeholder={'Введите email'} name={'email'} validate={[required]} component={Input}/>
+                        </div>
+                        <div className="input-field">
+                            <Field placeholder={'Введите пароль'} name={'password'} validate={[required]} component={Input} type={"password"}/>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-action">
+                    <button className='btn yellow darken-4 button-auth-log'>Войти</button>
+                    <button className='btn grey lighten-1 black-text'>Регистрация</button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+const AuthReduxForm = reduxForm({form: 'auth'})(AuthForm)
 
 const Auth = props => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
     return (
         <div className="row">
             <div className="col s6 offset-s3">
-                <div className="card blue darken-1">
-                    <div className="card-content white-text">
-                        <span className="card-title">Авторизация</span>
-                        <div>
-                            <div className="input-field">
-                                <input placeholder="Введите email" id="email" type="text"/>
-                                <label htmlFor="email">Email</label>
-                            </div>
-
-                            <div className="input-field">
-                                <input placeholder="Введите пароль" id="password" type="password"/>
-                                <label htmlFor="password">Пароль</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-action">
-                        <button className='btn yellow darken-4 button-auth-log'>Войти</button>
-                        <button className='btn grey lighten-1 black-text'>Регистрация</button>
-                    </div>
-                </div>
+                <AuthReduxForm onSubmit={onSubmit}/>
             </div>
         </div>
     )
