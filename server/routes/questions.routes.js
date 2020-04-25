619428452c08ca95dbb294be9c4fc68b52bb581c
SergeySolod <solodsergey287@gmail.com>
2020-04-25 23:07:30 +0300

    Questions routes

---
+++ server/routes/questions.routes.js
@@ -0,0 +1,29 @@
+const {Routes} = require('express')
+const Question = require('../models/Question')
+const router = Router()
+
+router.post('/generate', async (req, res) => {
+    try {
+
+    } catch (e) {
+        res.status(500).json({message: 'Что-то пошло не так, попробуйте снова'})
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+
+    } catch (e) {
+        res.status(500).json({message: 'Что-то пошло не так, попробуйте снова'})
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+
+    } catch (e) {
+        res.status(500).json({message: 'Что-то пошло не так, попробуйте снова'})
+    }
+})
+
+module.exports = router
