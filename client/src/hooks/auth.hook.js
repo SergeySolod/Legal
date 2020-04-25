a646992ff93b48a25cbdccc2353f1cad483f6f4e
SergeySolod <solodsergey287@gmail.com>
2020-04-25 16:34:48 +0300

    useAuth add login and logout

--- client/src/hooks/auth.hook.js
+++ client/src/hooks/auth.hook.js
@@ -1,7 +1,34 @@
-import {useState, useCallback} from 'react'
+import {useState, useCallback, useEffect} from 'react'
+
+const storageName = 'userData'
 
 export const useAuth = () => {
     //хранить jwttoken будем в LocalStorage, перезагружаем систему, есть есть валидный токен, то сразу допускаем пользователя в систему
     const [token, setToken] = useState(null)
     const [userId, setUserId] = useState(null)
+
+    const login = useCallback((jwtToken, id) => {
+        setToken(jwtToken)
+        setUserId(id)
+
+        localStorage.setItem(storageName, JSON.stringify({
+            userId, token
+        }))
+    }, [])
+
+    const logout = useCallback(() => {
+        setToken(null)
+        setUserId(null)
+
+        localStorage.removeItem(storageName)
+    }, [])
+
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem(storageName))
+        if (data && data.token) {
+            login(data.token, data.userId)
+        }
+    }, [])
+
+    return {login, logout, token, userId}
 }
