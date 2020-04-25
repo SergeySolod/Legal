bc96551b5a1c0eace7ec48fd67de5ca51d2b5a69
SergeySolod <solodsergey287@gmail.com>
2020-04-26 02:22:50 +0300

    Add baseUrl

--- server/routes/questions.routes.js
+++ server/routes/questions.routes.js
@@ -1,9 +1,12 @@
 const {Routes} = require('express')
 const Question = require('../models/Question')
 const router = Router()
+const config = require('config')
+const auth = require('../middleware/auth.middleware')
 
 router.post('/generate', async (req, res) => {
     try {
+        const baseUrl = config.get('baseUrl')
 
     } catch (e) {
         res.status(500).json({message: 'Что-то пошло не так, попробуйте снова'})
@@ -13,7 +16,7 @@ router.post('/generate', async (req, res) => {
 router.get('/my', async (req, res) => {
     try {
         //ждём пока модель Question найдёт все вопросы которые относятся к текущему пользователю
-        const questions = await Question.find({ owner: null }) // ???
+        const questions = await Question.find({owner: req.user.userId}) // ???
         res.json(questions)
     } catch (e) {
         res.status(500).json({message: 'Что-то пошло не так, попробуйте снова'})
