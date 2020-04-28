247654d43460469b1bab356a439b487e1e113c69
SergeySolod <solodsergey287@gmail.com>
2020-04-28 12:04:33 +0300

    Add fonts and bootstrap

--- mobile/App.js
+++ mobile/App.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {AppLoading} from 'expo'
+import {bootstrap} from "./src/bootstrap";
 
 export default function App() {
     const [isReady, setIsReady] = useState(false)
 
     if (!isReady) {
         return <AppLoading
+            //сюда передаём то, что будет делать загрузку, для загрузки будет отдельный файл
+            startAsync={bootstrap}
             //когда закончилась загрузка можем вызвать ещё функцию
             onFinish={() => setIsReady(true)}
             //обработка ошибки если она есть
---
+++ mobile/src/bootstrap.js
@@ -0,0 +1,7 @@
+import * as Font from 'expo-font'
+
+export async function bootstrap() {
+    await Font.loadAsync({
+
+    })
+}
