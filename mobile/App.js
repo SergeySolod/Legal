28ea40ae45a123f99b94774e43a2ab5ea4d69ecf
SergeySolod <solodsergey287@gmail.com>
2020-04-28 11:54:07 +0300

    Add mobile AppLoading

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {AppLoading} from 'expo'
 
 export default function App() {
+    const [isReady, setIsReady] = useState(false)
+
+    if (!isReady) {
+        return <AppLoading
+            //когда закончилась загрузка можем вызвать ещё функцию
+            onFinish={() => setIsReady(true)}
+            //обработка ошибки если она есть
+            onError={err => console.log(err)}
+        />
+    }
+
     return (
         <View>
             <Text>Open up App.js to start working on your app!</Text>
